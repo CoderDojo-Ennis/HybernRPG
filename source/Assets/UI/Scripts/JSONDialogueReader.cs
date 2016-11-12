a4ba3552ba73dcfdb93407c10919f2e2733f6ffc
Sethanic <bperill16@gmail.com>
2016-11-12 11:51:29 +0000

    Added the Workbench Scene
    It's for my work while not destroying everyone else's

--- source/Assets/UI/Scripts/JSONDialogueReader.cs
+++ source/Assets/UI/Scripts/JSONDialogueReader.cs
@@ -3,13 +3,36 @@ using System.Collections;
 using System.IO;
 using LitJson;
 
+public class Dialogue
+{
+    int ID { get; set; }
+    string Text { get; set; }
+}
+
+
 public class JSONDialogueReader : MonoBehaviour {
     private JsonData dialogueData;
 
+    public GameObject DialogueTextUI;
+
 	void Start () 
     {
+        //Dialogue speech = new Dialogue();
         dialogueData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/JSON/dialogue.json"));
+        SearchByID(6);
 	}
+
+    string SearchByID (int id)
+    {
+        for (int i = 0; i < dialogueData.Count; i++)
+        {
+            if (id == i)
+            {
+                return;
+                //Debug.Log(i.ToString());
+            }
+        }
+    }
 	
 	void Update () 
     {
