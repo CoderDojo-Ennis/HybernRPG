68e20363991dd63757542188a738db346a1b4c5e
fionnphotography <fionnm97@gmail.com>
2016-12-03 11:51:05 +0000

    I added a camera follow script

---
+++ source/Assets/Player/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform Player;
+	//public Transform startMarker;
+	//public Transform endMarker;
+	public float speed = .1F;
+	//private float startTime;
+	//private float journeyLength;
+
+	// Use this for initialization
+	void Start () {
+		
+	//	startTime = Time.time;
+	//	journeyLength = Vector3.Distance (startMarker.position, endMarker.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this.transform.position = Player.position
+	//	float distCovered = (Time.time - startTime) * speed;
+	//	float fracJourney = distCovered / journeyLength;
+		transform.position = Vector3.Lerp (transform.position, Player.position, speed);
+		this.transform.Translate (0, 0, -10);
+	}
+}
