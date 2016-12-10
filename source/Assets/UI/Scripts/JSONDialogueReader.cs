fe1cc6b7f5f807c22acb283b25473b320e6227d6
Sethanic <cian.perill@gmail.com>
2016-12-10 00:02:29 +0000

    More dialogue stuff
    Need help debugging this

--- source/Assets/UI/Scripts/JSONDialogueReader.cs
+++ source/Assets/UI/Scripts/JSONDialogueReader.cs
@@ -14,7 +14,12 @@ public class Dialogue
 public class JSONDialogueReader : MonoBehaviour {
     private string textData;
     private JsonData dialogueData;
+    private string DisplaySpeaker;
+    private string DisplayID;
+    private string NextSpeaker;
+    private string NextID;
 
+    public Button ContinueButton;
     public GameObject DialogueTextUI;
     public GameObject SpeakerTextUI;
     
@@ -23,10 +28,10 @@ public class JSONDialogueReader : MonoBehaviour {
         DialogueTextUI.transform.parent.gameObject.SetActive(false);
         textData = File.ReadAllText(Application.dataPath + "/JSON/dialogue.json");
         dialogueData = JsonMapper.ToObject(textData);
+        ContinueButton.GetComponent<Button>().onClick.AddListener(ContinueButtonFunction);
         //Debug
-        Debug.Log(GetText("Player", "0"));
-        DisplayDialogue("Player", "1"); // Latest DisplayDialogue called appears.
-
+        //Debug.Log(GetText("Player", "0"));
+        DisplayDialogue("Player", "0"); // Latest DisplayDialogue called appears.
 	}
 
 	string GetText (string speaker, string id) //Searches dialogue.json for text. Can be debugged like shown in Start().
@@ -40,6 +45,8 @@ public class JSONDialogueReader : MonoBehaviour {
     }
     void DisplayDialogue (string speaker, string id) //Uses GetText to find the text needed and displays it. Can be called like shown in Start(). 
     {
+        DisplaySpeaker = speaker;
+        DisplayID = id;
         if (!DialogueTextUI.transform.parent.gameObject.activeSelf)
         {
             DialogueTextUI.transform.parent.gameObject.SetActive(true);
@@ -47,5 +54,33 @@ public class JSONDialogueReader : MonoBehaviour {
         }
         DialogueTextUI.GetComponent<Text>().text = GetText(speaker, id);
         SpeakerTextUI.GetComponent<Text>().text = speaker;
+    }
+    bool GetNext(string speaker, string id) //Searches dialogue.json for the next piece of text in a conversation. Can likely be debugged in Start().
+    {
+        for (int i = 0; i < dialogueData[speaker].Count; i++)
+        {
+            if (dialogueData[speaker][i]["id"].ToString() == id)
+            {
+                Debug.Log("FUNCTION CALLED");
+                if (dialogueData[speaker][i]["next"] != null)
+                {
+                    Debug.Log("FUNCTION CALLED");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    void ContinueButtonFunction ()
+    {
+        
+        if (GetNext(DisplaySpeaker, DisplayID))
+        {
+
+        }
+        else
+        {
+            DialogueTextUI.SetActive(false);
+        }
     }
 }
