302b5d73036dbee95eb14e6f738110d40f93797b
James <jammysquirrelstudios@gmail.com>
2017-03-05 14:43:02 +0000

    Tidied up the assets folder, as things were getting *really* messy.

---
+++ source/Assets/Camera/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public Transform Player;
+	public float lerpSpeed = 0.1f;
+	
+	void FixedUpdate ()
+	{
+		//Lerp to player's positon
+		transform.position = Vector3.Lerp (transform.position, Player.position, lerpSpeed);
+		//Offset slightly on y axis
+		this.transform.Translate (0, 0, -10);
+	}
+}
