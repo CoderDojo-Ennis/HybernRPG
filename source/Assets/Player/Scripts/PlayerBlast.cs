854c7e418369c6810650ce543a11c4a7bd9b5d8c
James <jammysquirrelstudios@gmail.com>
2017-04-09 19:56:32 +0100

    Added design document for laser arm cultist Changed Cannon Blast sprites

--- source/Assets/Player/Scripts/PlayerBlast.cs
+++ source/Assets/Player/Scripts/PlayerBlast.cs
@@ -8,6 +8,13 @@ public class PlayerBlast : MonoBehaviour {
 	{
 		GetComponent<Rigidbody2D>().AddForce(this.transform.rotation * new Vector3(0, -5, 0),ForceMode2D.Impulse);
 	}
+	void Update()
+	{
+		Vector2 velocity = GetComponent<Rigidbody2D>().velocity;
+		float angle = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg;
+		
+		transform.rotation = Quaternion.AngleAxis (angle+90 , Vector3.forward);
+	}
 	void OnCollisionEnter2D()
 	{
 		GameObject.Destroy(this.gameObject);
