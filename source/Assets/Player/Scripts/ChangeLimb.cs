d3e4e975998f7a46c89cec171a55cdb89ad3f5ff
James <jammysquirrelstudios@gmail.com>
2017-04-12 22:03:48 +0100

    LIMB SWITCHING!!!

---
+++ source/Assets/Player/Scripts/ChangeLimb.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLimb : MonoBehaviour {
+
+	private GameObject child;
+	
+	void Start ()
+	{
+		child = transform.GetChild(0).gameObject;
+	}
+	
+	void Update ()
+	{
+		AnimationControl animationControl;
+		animationControl = child.GetComponent<AnimationControl>();
+		
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+            animationControl.ArmLimbs = 7;
+        }
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+            animationControl.ArmLimbs = 3;
+        }
+		if (Input.GetKeyDown(KeyCode.Alpha0))
+		{
+            animationControl.ArmLimbs = 0;
+        }
+	}
+}
