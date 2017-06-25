e65e2d229a4af94e0bdbe4872dfa9b2ce9698d7b
DRFIONN\Owner-PC <fionnthegamer@outlook.ie>
2017-06-25 10:52:32 +0100

    New portal animation and sound effect.

---
+++ source/Assets/Portal Animation stuff/PlayVideo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayVideo : MonoBehaviour {
+
+    public MovieTexture movie;
+	
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        movie.Play();
+	}
+	
+	
+	void Update () {
+		
+	}
+}
