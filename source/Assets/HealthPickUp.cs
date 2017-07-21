7140d3c342e25b490d893cd40f827da98df0ea23
Joey <joeyecorbett@Gmail.com>
2017-07-21 22:55:03 +0100

    made health pick up a little better

--- source/Assets/HealthPickUp.cs
+++ source/Assets/HealthPickUp.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class HealthPickUp : MonoBehaviour {
     float y;
-    
+    public float bobbingDistance;
+
+    [Range(0.0f, 1.0f)]
+    public float maxGlow;
+
     public void OnTriggerEnter2D(Collider2D coll) {
         if(coll.gameObject.name == "Player Physics Parent") {
             coll.gameObject.GetComponent<PlayerStats>().TakeDamage(-1);
@@ -19,12 +23,12 @@ public class HealthPickUp : MonoBehaviour {
 	void Update () {
         SpriteRenderer renderer = GetComponent<SpriteRenderer>();
         Material mat = renderer.material;
-        float emission = Mathf.PingPong(Time.time, 1.1f);
+        float emission = Mathf.PingPong(Time.time, maxGlow);
         Color baseColor = Color.green;
         Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
         mat.SetColor ("_EmissionColor", finalColor);
 
-        transform.position = new Vector3(transform.position.x, y + Mathf.PingPong(Time.time / 8, 0.2f), transform.position.z);
+        transform.position = new Vector3(transform.position.x, y + Mathf.PingPong(Time.time / 8, bobbingDistance), transform.position.z);
         //Vector3.Lerp(new Vector3(1f, 1f, 1f), new Vector3(explodeScale, 1f, explodeScale), lerp);
     }
 }
