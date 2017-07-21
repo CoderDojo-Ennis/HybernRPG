6d712a922535a0250f13f36712343fc63fdaf05d
James <jammysquirrelstudios@gmail.com>
2017-07-21 17:57:11 +0100

    Reorganised assets folder.

---
+++ source/Assets/_World1/Terrain/Scripts/Water.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if(collider.gameObject.name == "Player Physics Parent")
+		{
+			collider.gameObject.GetComponent<movement>().inWater = true;
+		}
+	}
+	void OnTriggerExit2D(Collider2D collider)
+	{
+		if(collider.gameObject.name == "Player Physics Parent")
+		{
+			collider.gameObject.GetComponent<movement>().inWater = false;
+		}
+	}
+}
