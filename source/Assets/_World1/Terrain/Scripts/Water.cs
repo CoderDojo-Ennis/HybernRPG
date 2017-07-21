ca929bdd12e4d33436619af3cf5b73bbc2dcc89a
James <jammysquirrelstudios@gmail.com>
2017-07-21 21:37:50 +0100

    Splashy fun in the water.

--- source/Assets/_World1/Terrain/Scripts/Water.cs
+++ source/Assets/_World1/Terrain/Scripts/Water.cs
@@ -6,6 +6,20 @@ public class Water : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D collider)
 	{
+		//Play a water splash sound
+		if(collider.gameObject.GetComponent<Rigidbody2D>())
+		{
+			float fallSpeed;
+			fallSpeed = collider.gameObject.GetComponent<Rigidbody2D>().velocity.y;
+			
+			if(fallSpeed < -4.4){
+				GameObject.Find("AudioManager").GetComponent<AudioManager>().Play("Water Splash");
+			}
+			else{
+				GameObject.Find("AudioManager").GetComponent<AudioManager>().Play("Water Splash 2");
+			}
+		}
+		
 		if(collider.gameObject.name == "Player Physics Parent")
 		{
 			collider.gameObject.GetComponent<movement>().inWater = true;
