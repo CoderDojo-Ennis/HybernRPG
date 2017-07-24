e9c4ce8789e32715e1694990583f1276fca60647
DRFIONN\Owner-PC <fionnthegamer@outlook.ie>
2017-07-24 18:26:07 +0100

    no message

---
+++ source/Assets/CheckPointController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour {
+
+    public Sprite redFlag;
+    public Sprite greenFlag;
+    private SpriteRenderer checkpointSpriteRenderer;
+    public bool checkpointReached;
+
+	// Use this for initialization
+	void Start () {
+        checkpointSpriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player") {
+            checkpointSpriteRenderer.sprite = greenFlag;
+            checkpointReached = true;
+        }
+    }
+        
+    }
+
