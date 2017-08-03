4ad7320f3344aa1e34f7426d350cbd17b31c76c3
James <jammysquirrelstudios@gmail.com>
2017-08-03 17:57:13 +0100

    Improved Nuclear Mushrooms.

---
+++ source/Assets/_General/Enemies/NuclearShroom/Scripts/ShroomExplosion.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShroomExplosion : MonoBehaviour {
+	private CameraFollow cameraFollow;
+	void Awake ()
+	{
+		//find camera script
+		cameraFollow = GameObject.Find("Main Camera").GetComponent<CameraFollow>();
+		
+		//Shake the screen
+		StartCoroutine(cameraFollow.MyRoutine(1f, 0.3f, 0.3f));
+		
+		//Destroy the explosion object and turn off its collider after a period of time
+		GameObject.Destroy ( gameObject, 4);
+		
+		this.Delay(0.2f, () => {
+                GetComponent< Collider2D >().enabled = false;
+            });
+	}
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		if( collider.gameObject.name == "Player Physics Parent" )
+		{
+			PlayerStats playerStats;
+			playerStats = collider.gameObject.GetComponent< PlayerStats >();
+			
+			playerStats.TakeDamage ( playerStats.health );
+		}
+		if( collider.gameObject.GetComponent< ShroomFire >() != null )
+		{
+			ShroomFire shroomFire;
+			shroomFire = collider.gameObject.GetComponent< ShroomFire >();
+			
+			float delay = Random.Range(0.2f, 1);
+			
+			this.Delay( delay, () => {
+                shroomFire.BlowUp ();
+            });
+		}
+		if( collider.gameObject.GetComponent< EnemyFramework >() != null )
+		{
+			EnemyFramework enemy;
+			enemy = collider.gameObject.GetComponent< EnemyFramework >();
+			
+			enemy.TakeDamage ( enemy.health );
+		}
+	}
+}
--- source/Assets/_General/Enemies/NuclearShroom/Scripts/ShroomFire.cs
+++ source/Assets/_General/Enemies/NuclearShroom/Scripts/ShroomFire.cs
@@ -4,46 +4,36 @@ using UnityEngine;
 using System.Linq;
 using UnityStandardAssets._2D;
 public class ShroomFire : MonoBehaviour {
-    public Animator anim;
+    public GameObject explosionPrefab;
+	
+	public Animator anim;
     public float visionRange;
-    public CameraFollow cameraFollow;
     public WhiteFlash whiteFlash;
     public DarkHaze darkHaze;
     public SpriteRenderer sr;
     private float lerp = 0;
     public float explodeScale;
     void Start () {
-        cameraFollow = GameObject.Find("Main Camera").GetComponent<CameraFollow>();
         whiteFlash = GameObject.Find("WhiteExplosionEffect").GetComponent<WhiteFlash>();
         darkHaze = GameObject.Find("AfterExplosionEffect").GetComponent<DarkHaze>();
         sr = GetComponentInChildren<SpriteRenderer>();
     }
 
-	void Update () {
+	void Update ()
+	{
 		if(anim.GetBool("fire") == true) {
-            anim.Play("fire");
+            //anim.Play("fire");
             lerp += Time.deltaTime;
             sr.color = Color.Lerp(Color.white, Color.red, lerp);
-            transform.localScale = Vector3.Lerp(new Vector3(1f, 1f, 1f), new Vector3(explodeScale, 1f, explodeScale), lerp);
-        }
-        if (lerp >= 1f) {
-            whiteFlash.Explode();
-            StartCoroutine(cameraFollow.MyRoutine(2f, 0.1f, 0.1f));
-            this.Delay(1f, destroy);
-            darkHaze.Explode();
+            //transform.localScale = Vector3.Lerp(new Vector3(1f, 1f, 1f), new Vector3(explodeScale, 1f, explodeScale), lerp);
         }
     }
 
     public void OnCollisionEnter2D(Collision2D coll) {
         if (coll.gameObject.tag == "Good") {
-            anim.SetBool("fire", true);
-//<<<<<<< Updated upstream
-            //GetComponentInChildren<SpriteRenderer>().color = Color.white;
-
-            this.Delay(0.25f, () => {
-                GameObject.Find("AudioManager").GetComponent<AudioManager>().Play("Nuclear Explosion");
-            });
-            this.Delay(1f, coll.gameObject.GetComponent<PlayerStats>().Die);/*
+			BlowUp ();
+		
+		/*
             this.Delay(2f, () => {
                 burn(coll.gameObject);
             });
@@ -56,17 +46,12 @@ public class ShroomFire : MonoBehaviour {
             StartCoroutine(cameraFollow.MyRoutine(5f, 0.1f, 0.1f));
             */
         }
+		if( coll.gameObject.GetComponent< EnemyFramework >() != null )
+		{
+			BlowUp();
+		}
     }
-
-    public void OnCollisionExit2D(Collision2D coll) {
-        if (coll.gameObject.tag == "Good") {
-            
-            anim.SetBool("fire", false);
-        }
-    }
-    private void destroy() {
-        Destroy(gameObject);
-    }
+	
     //<<<<<<< Updated upstream
     public void burn(GameObject go) {
         SpriteRenderer[] spriteR;
@@ -104,4 +89,37 @@ public class ShroomFire : MonoBehaviour {
             anim.SetBool("charge", true);
         }
     }
+	void OnTriggerEnter2D ( Collider2D collider )
+	{
+		if( collider.gameObject.name == "Player Physics Parent")
+		{
+			StartCoroutine ( Countdown () );
+		}
+		if( collider.gameObject.GetComponent< EnemyFramework >() != null )
+		{
+			StartCoroutine ( Countdown () );
+		}
+	}
+	public void BlowUp()
+	{
+		//Don't call any more explosions
+		StopAllCoroutines ();
+        
+		//Play sound effect
+		GameObject.Find("AudioManager").GetComponent<AudioManager>().Play("Nuclear Explosion");
+		
+		//Blow up the shroom
+		GameObject.Destroy(gameObject,0.01f);
+		Instantiate( explosionPrefab, transform.position, Quaternion.identity);
+	}
+	public IEnumerator Countdown ()
+	{
+		anim.SetBool("charge", true);
+		anim.SetBool("fire", true);
+		GetComponentInChildren<SpriteRenderer>().color = Color.white;
+			
+		yield return new WaitForSeconds (1f);
+		
+		BlowUp ();
+	}
 }
