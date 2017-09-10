acab55581bc5600ce2bb3c941654354c61b4c3c0
James <jammysquirrelstudios@gmail.com>
2017-09-10 17:58:10 +0100

    Created new level. Improved intro (instructions on how to change limbs)

---
+++ source/Assets/_World1/Terrain/Scripts/AnimationTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour {
+	
+	public string ID;
+	public string Speaker;
+	
+	private JSONDialogueReader reader;
+	
+	void Start () {
+		reader = GameObject.Find("TextBoxCanvas").GetComponent<JSONDialogueReader>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Speaker == reader.DisplaySpeaker && ID == reader.DisplayID)
+		{
+			GetComponent<Animator>().enabled = true;
+			
+			Destroy(this);
+		}
+		
+	}
+}
