d657ac8309fa8010f9beafe86c6fd519b4451a77
Unknown <cian.perill@gmail.com>
2017-11-10 23:20:44 +0000

    Made a scene for Connor's execution

---
+++ source/Assets/_World5/cutscenes/Scripts/ExecutionManagement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ExecutionManagement : MonoBehaviour
+{
+	public float desiredSpeed;
+	public float lerpSpeed;
+	
+	private Rigidbody2D rb;
+	void OnEnable () {
+		rb = GetComponent<Rigidbody2D>();
+		rb.bodyType = RigidbodyType2D.Kinematic;
+	}
+	
+	void Update () {
+		Vector2 target;
+		target = new Vector2(0, desiredSpeed);
+		
+		Vector2 result;
+		result = Vector2.Lerp(rb.velocity, target, lerpSpeed);
+		
+		rb.velocity = result;
+	}
+}
