4e38718c88b8034344f979bdbbbc9d280eb6d22f
James <jammysquirrelstudios@gmail.com>
2017-12-09 12:44:09 +0000

    Fixed lag with pickaxe arms.

--- source/Assets/_General/Player/Scripts/PickaxeAttack.cs
+++ source/Assets/_General/Player/Scripts/PickaxeAttack.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PickaxeAttack : MonoBehaviour {
-
+	
+	Pickaxes pickaxes;
+	
+	void OnEnable ()
+	{
+		pickaxes = transform.parent.gameObject.GetComponent<Pickaxes>();
+	}
+	
 	void OnTriggerEnter2D ( Collider2D collider )
 	{
-		EnemyFramework enemy = collider.GetComponent<EnemyFramework>();
-		if( enemy != null)
+		if( pickaxes.pickaxesEnabled && pickaxes.enabled )
 		{
-			enemy.TakeDamage(1);
-			return;
-		}
-		if(collider.gameObject.name == "überCultist")
-		{
-			collider.gameObject.GetComponent<UberCultistBehaviour>().TakeDamage(1);
+			EnemyFramework enemy = collider.GetComponent<EnemyFramework>();
+			if( enemy != null)
+			{
+				enemy.TakeDamage(1);
+				return;
+			}
+			if(collider.gameObject.name == "überCultist")
+			{
+				collider.gameObject.GetComponent<UberCultistBehaviour>().TakeDamage(1);
+			}
 		}
 	}
 }
