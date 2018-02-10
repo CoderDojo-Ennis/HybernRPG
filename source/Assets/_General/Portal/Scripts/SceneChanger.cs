75560c91ab2a82003d4df831e0265bd0fc97895f
James <jammysquirrelstudios@gmail.com>
2018-02-10 13:44:32 +0000

    Health orb glitch fixed, Melee cultists no longer jump on player's head, fixed issue with portal.

--- source/Assets/_General/Portal/Scripts/SceneChanger.cs
+++ source/Assets/_General/Portal/Scripts/SceneChanger.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 public class SceneChanger : MonoBehaviour {
     public Image black;
 
-    public string currentIndex { get; private set; }
-
     void OnTriggerEnter2D(Collider2D coll) {
 	
         if (coll.gameObject.name == "Player Physics Parent") {
@@ -22,8 +20,9 @@ public class SceneChanger : MonoBehaviour {
 	{
 		//Find current scene index
 		int currentIndex = SceneManager.GetActiveScene().buildIndex;
-    
+	
 		//Change to next scene in build
 		SceneManager.LoadSceneAsync(currentIndex + 1);
+		
 	}
 }
